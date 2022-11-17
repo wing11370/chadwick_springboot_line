dec56e13fd2c05399853e13a7ed6ba2a2d9ae6db
Chadwick <fcw11370@gmail.com>
2022-11-17 20:53:58 +0800

    setup a Sprint Boot project

---
+++ src/main/java/com/example/chadwick_springboot_line/ChadwickSpringbootLineApplication.java
@@ -0,0 +1,13 @@
+package com.example.chadwick_springboot_line;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChadwickSpringbootLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChadwickSpringbootLineApplication.class, args);
+	}
+
+}
